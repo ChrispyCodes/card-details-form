6915bb86a5b6f34cf58d523e98c4ab2abf6de40d
Christopher <thetoph@outlook.com>
2022-11-27 14:24:16 -0800

    updating main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Card from "./components/Card";
+import CardForm from "./components/CardForm";
+import CardSubmitted from "./components/CardSubmittedForm";
 import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState({
+    name: null,
+    cardNumber: null,
+    mm: null,
+    yy: null,
+    cvc: null,
+  });
+  const [errors, setErrors] = useState({
+    errorMessages: {
+      name: "",
+      cardNumber: "",
+      mm: "",
+      yy: "",
+      cvc: "",
+    },
+  });
+  const [validate, setValidate] = useState(false);
+
+  const formValidation = (validate) =>
+    setTimeout(() => {
+      setValidate(validate);
+      document.querySelector(".cardOverflow > div").style.transform =
+        "translate(0)";
+    }, 500);
 
   return (
-    <div>
+    <>
+      <Card formData={formData} />
+      <main className="cardOverflow">
+        <div>
+          {validate ? (
+            <CardSubmitted
+              setFormData={setFormData}
+              formValidation={formValidation}
+            />
+          ) : (
+            <CardForm
+              setFormData={setFormData}
+              formData={formData}
+              formValidation={formValidation}
+              errors={errors}
+              setErrors={setErrors}
+            />
+          )}
+        </div>
+      </main>
+
       <footer className="attribution">
         <p>
-          Made with ♥️ by{" "}
+          Made by{" "}
           <a
             href="https://github.com/chrispycodes"
             target="_blank"
@@ -19,7 +65,7 @@ function App() {
           </a>
         </p>
       </footer>
-    </div>
+    </>
   );
 }
 
